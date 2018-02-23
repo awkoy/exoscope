a2cd013a7b98553a0d2d05b950da6ec9d391bfc1
unknown <y.boikodevelop@gmail.comgit config --global user.name awkoy>
2018-02-23 19:38:21 +0200

    add episodes

--- src/containers/episodes.js
+++ src/containers/episodes.js
@@ -2,14 +2,9 @@ import React, { Component } from "react";
 import { Episode } from './../components';
 
 class Episodes extends Component {
-    getEpisodes = () => {
-        const episodeData = [{
-            tag: 'Tag name',
-            title: 'Title episode',
-            time: '10 min ago',
-            duration: '2 day'
-        },
-        {
+    constructor(props) {
+        super(props);
+        this.episodeData = [{
             tag: 'Tag name',
             title: 'Title episode',
             time: '10 min ago',
@@ -33,14 +28,22 @@ class Episodes extends Component {
             time: '10 min ago',
             duration: '2 day'
         }];
-        return episodeData.map((el,i) => <Episode key={i} tag={el.tag} title={el.title} time={el.time} duration={el.duration}/>);
+    }
+
+    getEpisodes = () => {        
+        return this.episodeData.map((el,i) => (i < this.episodeData.length - 2) && <Episode key={i} tag={el.tag} title={el.title} time={el.time} duration={el.duration}/>);
+    };
+    getLastEpisodes = () => {
+        return this.episodeData.map((el,i) => !(i < this.episodeData.length - 2) && <Episode key={i} tag={el.tag} title={el.title} time={el.time} duration={el.duration}/>);
     };
 
     render() {
       return (
-        <section className="episodes">
+        <section className="episode">
             <div className="container">
                 {this.getEpisodes()}
+                <div className="episode__last">{this.getLastEpisodes()}</div>
+                <div className="episode__add">Ad block</div>
             </div>
         </section>
       );
